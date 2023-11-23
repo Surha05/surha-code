757d0618adad6e0549fe7777b68a3ae417ef281a
Surha <surha_dag@mail.ru>
2023-11-23 18:39:27 +0300

    перевел index и header в phph

---
+++ modules/header/nav.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
+		<div class="container d-flex align-items-center">
+			<a class="navbar-brand" href="/">Surha-Code</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button>
+			<div class="collapse navbar-collapse" id="ftco-nav">
+				<ul class="navbar-nav ml-auto">
+					<!-- <li class="nav-item active"><a href="/" class="nav-link pl-0">Home</a></li> -->
+					<li class="nav-item"><a href="about.html" class="nav-link">О нас</a></li>
+					<li class="nav-item"><a href="teacher.html" class="nav-link">Ученики</a></li>
+					<li class="nav-item"><a href="courses.html" class="nav-link">Курсы</a></li>
+					<li class="nav-item"><a href="pricing.html" class="nav-link">Цены</a></li>
+					<li class="nav-item"><a href="blog.html" class="nav-link">Новости</a></li>
+					<li class="nav-item"><a href="contact.html" class="nav-link">Контакты</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
---
+++ modules/header/top-block.php
@@ -0,0 +1,26 @@
+<div class="py-2 bg-primary">
+  <div class="container">
+    <div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
+      <div class="col-lg-12 d-block">
+        <div class="row d-flex">
+          <div class="col-md-5 pr-4 d-flex topper align-items-center">
+            <div class="icon bg-fifth mr-2 d-flex justify-content-center align-items-center"><span class="icon-map"></span></div>
+            <span class="text">Дербент, ул. Пушкина 55</span>
+          </div>
+
+          <div class="col-md pr-4 d-flex topper align-items-center">
+
+            <div class="icon bg-secondary mr-2 d-flex justify-content-center align-items-center"><span class="icon-whatsapp"></span></div><a href="https://wa.me/79224338833" target="_blank">
+              <span class="text">WhatsApp</span>
+            </a>
+          </div>
+          <div class="col-md pr-4 d-flex topper align-items-center">
+            <div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+            <a href="tel:+79224338833">
+              <span class="text">8(922)433-88-33</span></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
