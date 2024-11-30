e464e1e0ec11d509a4a0ac7818ed433c66c986ac
Surha <surha_dag@mail.ru>
2024-12-01 00:11:13 +0300

    add telegram in header

--- modules/header/top-block.php
+++ modules/header/top-block.php
@@ -3,21 +3,29 @@
     <div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
       <div class="col-lg-12 d-block">
         <div class="row d-flex">
-          <div class="col-md-5 pr-4 d-flex topper align-items-center">
+          <div class="col-lg-3 col-md-6 pr-4 d-flex topper align-items-center">
             <div class="icon bg-fifth mr-2 d-flex justify-content-center align-items-center"><span class="icon-map"></span></div>
-            <span class="text">Дербент, Пушкина 66, (над скорой помощью)</span>
+            <span class="text">Дербент, Пушкина 66</span>
           </div>
 
-          <div class="col-md pr-4 d-flex topper align-items-center">
+          <div class="col-lg-3 col-md-6 pr-4 d-flex topper align-items-center">
+            <div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+            <a href="tel:+79224338833">
+              <span class="text">8(922)433-88-33</span></a>
+          </div>
+
+          <div class="col-lg-3 col-md-6 pr-4 d-flex topper align-items-center">
 
             <div class="icon bg-secondary mr-2 d-flex justify-content-center align-items-center"><span class="icon-whatsapp"></span></div><a href="https://wa.me/79224338833" target="_blank">
               <span class="text">WhatsApp</span>
             </a>
           </div>
-          <div class="col-md pr-4 d-flex topper align-items-center">
-            <div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
-            <a href="tel:+79224338833">
-              <span class="text">8(922)433-88-33</span></a>
+
+          <div class="col-lg-3 col-md-6 pr-4 d-flex topper align-items-center">
+
+            <div class="icon bg-dark mr-2 d-flex justify-content-center align-items-center"><span class="icon-telegram"></span></div><a href="https://t.me/Surha05" target="_blank">
+              <span class="text">Telegram</span>
+            </a>
           </div>
         </div>
       </div>
