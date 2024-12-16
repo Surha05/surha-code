c85cc90251cecef67afdda03b6ee1ffc32a928e6
Surha <surha_dag@mail.ru>
2024-12-16 23:41:20 +0300

    добавил favicon

--- modules/head/head.php
+++ modules/head/head.php
@@ -13,6 +13,7 @@
   <meta property="og:site_name" content="Surha-Code" />
   <meta property="og:locate" content="ru_RU" />
 
+  <link rel="icon" href="/images/logo/logo.jpg">
   <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great" rel="stylesheet">
 
