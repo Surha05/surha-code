615e9653536441291e0f3e3d0b4cf0162e0fff79
Surha <surha_dag@mail.ru>
2024-12-21 00:44:41 +0300

    правка работы Обратный звонок

--- modules/popup/callBack.js
+++ modules/popup/callBack.js
@@ -1,39 +1,39 @@
-(function(){
-    const wrapper = document.querySelector('#callback-popup')
-    const form = document.querySelector('#popup-form');
-    const name = form.querySelector('#inp-name')
-    const tel = form.querySelector('#inp-tel')
-    const course = form.querySelector('#inp-course')
-    const smena = form.querySelector('#inp-smena')
-    const formEducation = form.querySelector('#inp-form-education')
-    const comment = form.querySelector('#inp-comment')
-    const btn = form.querySelector('#popup-send')
-  
-    btn.addEventListener('click', send)
-  
-    function send(e) {
-      e.preventDefault();
-      let message = `Имя - ${name.value}
+(function () {
+  const wrapper = document.querySelector('#callback-popup');
+  const form = document.querySelector('#popup-form');
+  const name = form.querySelector('#inp-name');
+  const tel = form.querySelector('#inp-tel');
+  const course = form.querySelector('#inp-course');
+  const smena = form.querySelector('#inp-smena');
+  const formEducation = form.querySelector('#inp-form-education');
+  const comment = form.querySelector('#inp-comment');
+  const btn = form.querySelector('#popup-send');
+
+  btn.addEventListener('click', send);
+
+  function send(e) {
+    e.preventDefault();
+    let message = `Имя - ${name.value}
       Телефон - ${tel.value}
       Курс - ${course.value}
       Смена - ${smena.value}
       Форма обучения - ${formEducation.value}
-      Комментарий - ${comment.value}`
-  
-      const url = '/modules/callBack/mailing.php';
-      const form_data = new FormData();
-      form_data.append('message', message);
-      fetch(url, {
-        method: 'POST',
-        body: form_data,
+      Комментарий - ${comment.value}`;
+
+    const url = '/modules/callBack/mailing.php';
+    const form_data = new FormData();
+    form_data.append('message', message);
+    fetch(url, {
+      method: 'POST',
+      body: form_data,
+    })
+      .then((res) => {
+        form.reset();
+        alert('Мы вам перезвоним в ближайшее время');
+        wrapper.classList.remove('active');
       })
-        .then((res) => {
-          form.reset();
-          alert('Мы вам перезвоним в ближайшее время');
-          wrapper.classList.remove('active');
-        })
       .catch(() => {
-        alert('Произошла ошибка. Повторите еще раз')
+        alert('Произошла ошибка. Повторите еще раз');
       });
-    }
-  })();
+  }
+})();
